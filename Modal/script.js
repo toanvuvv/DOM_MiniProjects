c31ed5fb7a416a553e66c2c9ffb8c8924dde7967
toanvuvv <toan.vd205173@sis.hust.edu.vn>
2024-03-29 15:48:38 +0700

    done modal

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,24 @@
 'use strict';
+let modal = document.querySelector('.modal');
+let overlay = document.querySelector('.overlay');
+let btnCloseModal = document.querySelector('.close-modal');
+let btnsOpenModal = document.querySelectorAll('.show-modal');
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
